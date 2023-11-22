af68495a5fd42e8c034003c55290650ba3202977
Joaquin Badillo <joaquin.badillo.g@gmail.com>
2023-11-22 01:38:33 -0600

    Add simple behaviors to car agent:
    * Sensing stoplights at their cell
    
    * Determine if taking a turn soon
    
    * Check surroundings (get neighborhood)
    
    * Wait
    
    * Calculate Routes
    
    * Turn on the directionals

--- Backend/TrafficSimulation/agents.py
+++ Backend/TrafficSimulation/agents.py
@@ -5,33 +5,112 @@
 # Joaquín Badillo
 
 from mesa import Agent
-from enum import Enum
 
-# Road Directions
-Directions = Enum('Directions', ['UP', 'RIGHT', 'LEFT', 'DOWN'])
+from utilities import (
+    Directions,
+    Colors
+)
 
-# Stoplight Colors
-Colors = Enum('Colors', ['GREEN', 'YELLOW', 'RED'])
+from typing import List
 
 class Car(Agent):
-    def __init__(self, unique_id, model) -> None:
+    def __init__(self, unique_id, model, destination) -> None:
         super().__init__(unique_id, model)
         self.steps_taken = 0
+        self.destination = destination
+        self.route = self.model.finder.astar(self.pos, self.destination)
+        
+        self.turn = None
 
-    def move(self) -> None:
-        pass
+        self.model = model
+        self.arrived = False
 
-    def calculate_route(self) -> None:
-        pass
+    def action(self) -> None:
+        # -- State and Sensoring --
+        
+        # Stoplight (Currently sensed only in the same cell)
+        stoplight = next(
+            filter(
+                lambda x: type(x) == Stoplight, 
+                self.model.grid.get_cell_list_contents([self.pos])
+            ), 
+            None
+        )
+        
+        if stoplight is not None:
+            if stoplight.state == Colors.RED or stoplight.state == Colors.YELLOW:
+                self.wait()
+                return
+
+        # Turn directionals if taking a turn in at most 5 steps
+        if self.turn is None:
+            limit = 5
+            
+            for x, y in self.route:
+                if limit == 0: break
+
+                if x != self.pos[0]:
+                    if x < self.pos[0]:
+                        self.turn_directional(Directions.LEFT)
+                    else:
+                        self.turn_directional(Directions.RIGHT)
+                    break
+                elif y != self.pos[1]:
+                    if y < self.pos[1]:
+                        self.turn_directional(Directions.UP)
+                    else:
+                        self.turn_directional(Directions.DOWN)
+                    break
+
+                limit -= 1
+
+
+        neighbors = self.model.grid.get_neighborhood(self.pos, 
+                                                     moore=False, 
+                                                     include_center=False)
+
+        # -- Decision Making --
     
-    def turn_sidelight(self) -> None:
+        pass
+
+    def move(self, pos) -> None:
+        self.model.grid.move_agent(self, pos)
+
+    def calculate_route(self, blockages: List[int] = None) -> None:
+        temp = []
+
+        # Use custom blockages for A* to avoid clustered routes on recalculation
+        if blockages is not None:
+            for idx, position in enumerate(blockages):
+                agent = Obstacle(f"temp{idx}", self.model)
+                temp.append(agent)
+                self.model.grid.place_agent(agent, position)
+
+        path = self.model.finder.astar(self.pos, self.destination)
+        
+        if self.route is None:
+            self.route = path
+        
+        # Toleration for a 30% increase in route length
+        # TODO - May change this to a percentage based on behavioral states
+        elif len(path) < 1.3 * len(self.route):
+            self.route = path
+
+        # Temp Cleanup
+        for agent in temp:
+            self.model.grid.remove_agent(agent)
+    
+    def turn_directional(self, direction) -> None:
+        self.turn = direction
         pass
 
     def wait(self) -> None:
-        pass
+        # TODO - May change some state variables (tolerance meter or something)...
+        return
 
     def step(self) -> None:
-        self.move()
+        if self.arrived: return
+        self.action()
 
 class Destination(Agent):
     def __init__(self, unique_id, model) -> None:
